48ca46d83fcee5390dd6c3b17d053c1d62ba8235
Jopagaro <rocoroche13@gmail.com>
2026-04-22 12:26:43 -0400

    Initial Commit

---
+++ parkfriends/parkfriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  parkfriendsApp.swift
+//  parkfriends
+//
+//  Created by JohnPaul Roche on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct parkfriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
